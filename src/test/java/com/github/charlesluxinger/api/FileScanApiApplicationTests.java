fb3cd8177a430c20ae17ae1066f2720267d12a1b
Charles Luxinger <charles.luxinger@picpay.com>
2020-07-09 21:25:00 -0300

    Initialized project

---
+++ src/test/java/com/github/charlesluxinger/api/FileScanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlesluxinger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileScanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
