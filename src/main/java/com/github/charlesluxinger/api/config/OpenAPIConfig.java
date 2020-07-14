def335b381493b400ac91757f5ffedff310b6fc2
Charles Luxinger <charlesluxinger@hotmail.com>
2020-07-14 04:04:06 -0300

    Heroku config

--- src/main/java/com/github/charlesluxinger/api/config/OpenAPIConfig.java
+++ src/main/java/com/github/charlesluxinger/api/config/OpenAPIConfig.java
@@ -9,9 +9,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
         info = @Info(
                 title = "File Scan Api",
                 version = "v1.0.0",
-                description = "A test project with the objective of checking some Github repository and returning the number of files and the number of lines per group of files",
+                description = "A test project with the objective of checking some Github repository and returning the number of bits and number of lines per group of type files.",
                 contact = @Contact(name = "Charles Luxinger", email = "charlesluxinger@gmail.com")
         ),
-        servers = @Server(url = "http://localhost:9009/api/v1", description = "Dev Server")
+        servers = {@Server(url = "http://localhost:9009/api/v1", description = "Dev Server"),
+                   @Server(url = "http://file-scan-api.herokuapp.com/api/v1/", description = "Prod Server")}
 )
 public class OpenAPIConfig {}
