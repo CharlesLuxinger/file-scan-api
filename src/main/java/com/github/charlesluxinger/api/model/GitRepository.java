02ef826ab39d5122e8e6515877735b3005943aeb
Charles Luxinger <charlesluxinger@hotmail.com>
2020-07-14 04:20:57 -0300

    Heroku config

--- src/main/java/com/github/charlesluxinger/api/model/GitRepository.java
+++ src/main/java/com/github/charlesluxinger/api/model/GitRepository.java
@@ -1,6 +1,5 @@
 package com.github.charlesluxinger.api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,16 +9,14 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GitRepository {
 
-	@JsonIgnore
-	@Schema(hidden = true)
 	private final String GITHUB_URL_REGEX = "(https://github\\.com/)\\b[-a-zA-Z0-9()@:%_.+~#?&/=]+";
 
+	@Setter
+	@Getter
 	@Schema(example = "https://github.com/CharlesLuxinger/file-scan-api")
 	@NotBlank
 	@Pattern(regexp = GITHUB_URL_REGEX)
